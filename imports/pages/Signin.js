6ba3d4540ce9ccd933e43e10069ed84503347cc5
ccachoulet <57669318+ccachoulet@users.noreply.github.com>
2020-01-23 17:07:40 +0100

    Added withTracker on signin

--- imports/pages/Signin.js
+++ imports/pages/Signin.js
@@ -1,18 +1,23 @@
 import React , {useState} from 'react'
 import {Form , Button , Container} from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { withTracker } from 'meteor/react-meteor-data';
+
+
 
 function Signin(props) {
     const [email , setEmail]        = useState()
     const [password, setPassword]   = useState()
 
     const signin = () => {
-        Media.loginWithPassword(email , password)
+        Meteor.loginWithPassword(email , password)
     }
+    const logout = () => Meteor.logout()
 
     return(
         <Container>
             <h1>Connectez-vous</h1>
+            {!props.current_user ?
             <Form onSubmit={signin}>
                 <Form.Input onChange={(e, {value}) => setEmail(value)} label="Email"required type ="email" placeholder="ex : toto@yopmail.com" />
                 <Form.Input onChange={(e, {value}) => setPassword(value)} label="Mot de passe"required type="password" placeholder="Entrez votre mot de passe" />
@@ -21,8 +26,20 @@ function Signin(props) {
                     <Button size="mini">Créer un compte</Button>
                 </Link>
             </Form>
+        :
+            <div>
+                <p>Vous ètes déjà connecté</p>
+                <Button onClick={logout} color="red">Se déconnecter</Button>
+            </div>
+            }
         </Container>
     )
 }
 
-export default Signin
+
+export default withTracker(() => {
+    const current_user = Meteor.user()
+    return{
+        current_user
+    }
+})(Signin)
