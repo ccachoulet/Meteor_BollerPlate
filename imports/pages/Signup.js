a7dfa6c12069554af837d3134f5a063730ccc4a3
ccachoulet <57669318+ccachoulet@users.noreply.github.com>
2020-01-23 16:18:50 +0100

    Added signup process and Constellation package

--- imports/pages/Signup.js
+++ imports/pages/Signup.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React , {useState} from 'react'
 import {Form , Button , Container} from 'semantic-ui-react'
+import { Accounts } from 'meteor/accounts-base'
 
 function Signup(props) {
+    const [email , setEmail]        = useState()
+    const [password, setPassword]   = useState()
+
+    const signup = () => {
+        Accounts.createUser({
+            email,
+            password
+        }, (err) => { 
+            if(err){
+                alert(err.message)
+            }else{
+                console.log('Utilisateur crée')
+            }
+        })
+    }
+    
     return(
         <Container>
             <h1>Inscrivez-Vous !</h1>
-            <Form>
-                <Form.Input label="Email"required type="email" placeholder="ex : toto@yopmail.com" />
-                <Form.Input label="Mot de passe"required type="password" placeholder="Entrez votre mot de passe" />
-                <Button color="blue">M'inscrire</Button>
+            <Form onSubmit={signup}>
+                <Form.Input onChange={(e, {value}) => setEmail(value)} label="Email"required type ="email" placeholder="ex : toto@yopmail.com" />
+                <Form.Input onChange={(e, {value}) => setPassword(value)} label="Mot de passe"required type="password" placeholder="Entrez votre mot de passe" />
+                <Button disabled={!email || !password} color="blue">M'inscrire</Button>
             </Form>
         </Container>
     )
