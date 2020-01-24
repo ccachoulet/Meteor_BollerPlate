5433858cf1324d51db1479aa938b8a796ecef5db
ccachoulet <57669318+ccachoulet@users.noreply.github.com>
2020-01-24 11:25:10 +0100

    Created Products , SimpleSchema

---
+++ imports/api/products/Products.js
@@ -0,0 +1,38 @@
+import SimpleSchema, { MongoObject } from "simpl-schema"
+
+const Products = new Mongo.Collection('products')
+
+const ProductSchema = new SimpleSchema({
+    name : {
+        type : String
+    },
+    price : {
+        type : Number
+    },
+    description:{
+        type : String,
+        defaultValue : "Un produit"
+    },
+    seller : {
+        type :String
+    },
+    stock : {
+        type : Number,
+        defaultValue : 0
+    },
+    image_url : {
+        type : String,
+        defaultValue : "https://static.thenounproject.com/png/28189-200.png"
+    },
+    category : {
+        type : String,
+        optional : true
+    },
+    created_at : {
+        type : Date,
+        defaultValue : NewDate()
+    }
+})
+
+Products.attachSchema(ProductSchema)
+
